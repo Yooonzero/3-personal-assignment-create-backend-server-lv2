01a63a1ecd305c0695f7487c3e941cbeb77931ea
yooonzero <playlist808@kakao.com>
2023-06-22 01:43:44 +0900

    댓글 수정 및 삭제 기능 추가

--- routes/comments.js
+++ routes/comments.js
@@ -39,11 +39,11 @@ router.post('/:postId/comments', async (req, res) => {
 router.get('/:postId/comments', async (req, res) => {
     const { postId } = req.params;
     try {
-        const post = Posts.findOne({ _id: postId });
+        const post = await Posts.findOne({ _id: postId });
         if (!post) {
             return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
         }
-        const comments = Comments.find({ postId: postId });
+        const comments = await Comments.find({ postId: postId });
         const data = {
             comments: comments.map((a) => {
                 return {
@@ -64,40 +64,64 @@ router.get('/:postId/comments', async (req, res) => {
 });
 
 // 댓글 수정
-router.put('/:postId/comments/:commentId', async (req, res) => {
-    const { _id } = req.params;
-    console.log(_id);
-    const { password, content } = req.body;
-    const existComment = await Comments.findOne({ _id });
-    console.log(existComment);
-    if (existComment && password === existComment.password) {
-        await Comments.updateOne({ password: password }, { $set: { content: content } });
-        return res.status(200).send({ msg: '댓글을 수정했습니다.' });
-
-        // console.log(error);
-        // res.status(400).json({ msg: '되나?' });
+router.put('/:postId/comments/:commentId', authMiddleware, async (req, res) => {
+    const { postId, commentId } = req.params;
+    const { comment } = req.body;
+    const { _id } = res.locals.user;
+    if (Object.keys(req.body).length === 0 || Object.value(req.params).length === 0) {
+        return res.status(412).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
     }
-    if (!content) {
-        res.status(400).send({ msg: '댓글 내용을 입력해주세요.' });
+    if (comment.trim() === '' || undefined) {
+        return res.status(400).json({ errorMessage: '댓글 내용을 입력해주세요.' });
     }
-    if (_id !== existComment._id || password !== existComment.password) {
-        res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
+    try {
+        const post = await Posts.findOne({ _id: postId });
+        if (!post) {
+            return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        }
+        const existComment = await Comments.findById(commentId);
+        if (!existComment) {
+            return res.status(404).json({ errorMessage: '댓글이 존재하지 않습니다.' });
+        }
+        if (existComment.userId !== _id.toString()) {
+            return res.status(403).json({ errorMessage: '댓글 수정 권한이 존재하지 않습니다.' });
+        }
+        await Comments.updateOne({ _id: commentId }, { $set: { comment: comment } }).catch((err) => {
+            res.status(400).json({ errorMessage: '댓글 수정이 처리되지 않았습니다.' });
+        });
+        res.status(200).json({ errorMessage: '댓글이 성공적으로 수정되었습니다.' });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({ errorMessage: '댓글 수정에 실패하였습니다.' });
     }
 });
 
 // 댓글 삭제
-router.delete('/:postId/comments', async (req, res) => {
-    const { _id } = req.params;
-    const { password } = req.body;
-    const existComment = await Comments.findOne({ _id });
-    if (existComment && String(existComment.password) === password) {
-        await Comments.deleteOne({ _id });
-        res.status(200).send({ msg: '댓글을 삭제하였습니다.' });
+router.delete('/:postId/comments/commetId', authMiddleware, async (req, res) => {
+    const { postId, commentId } = req.params;
+    const { comment } = req.body;
+    const { _id } = res.locals.user;
+
+    try {
+        const post = await Posts.findOne({ _id: postId });
+        if (!post) {
+            return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        }
+        const existComment = Comments.findById(commentId);
+        if (!existComment) {
+            return res.status(404).json({ errorMessage: '댓글이 존재하지 않습니다.' });
+        }
+        if (existComment.userId !== _id.toString()) {
+            return res.status(403).json({ errorMessage: '댓글 삭제 권한이 존재하지 않습니다.' });
+        }
+        await Comments.deleteOne({ _id: commentId }).catch((err) => {
+            res.status(400).json({ errorMessage: '댓글 삭제가 정상적으로 처리되지 않았습니다.' });
+        });
+        res.status(200).json({ errorMessage: '댓글 삭제에 성공하였습니다.' });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({ errorMessage: '댓글 삭제에 실패하였습니다.' });
     }
-    if (password !== String(existComment.password)) {
-        res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
-    }
-    // res.status(404).send({ msg: '댓글 조회에 실패하였습니다.' });
 });
 
 module.exports = router;
