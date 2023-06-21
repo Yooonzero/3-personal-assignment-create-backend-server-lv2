9d9e311d2440ce3c89ec2bc7c8c1c90706a0cffb
yooonzero <playlist808@kakao.com>
2023-06-22 01:18:26 +0900

    댓글 생성 및 조회 기능추가

--- routes/comments.js
+++ routes/comments.js
@@ -4,43 +4,67 @@ const Posts = require('../schemas/post.js');
 const Comments = require('../schemas/comment.js');
 const authMiddleware = require('../middlewares/auth-middleware.js');
 
-// 댓글 작성
-router.post('/comments/:postId', async (req, res) => {
+// 1. 댓글 작성
+router.post('/:postId/comments', async (req, res) => {
     const { postId } = req.params;
-    const { user, password, content } = req.body;
+    const { content } = req.body;
+    const { nickname, _id } = res.locals.user;
+
+    if (req.body.length === 0) {
+        return res.status(400).json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
+    }
+    if (!content || content.trim() === '') {
+        return res.status(400).json({ errorMessage: '댓글 내용을 입력해 주십시오.' });
+    }
+
     try {
-        const createdComment = await Comments.create({
-            postId,
-            user,
-            password, // body에 0이라는 숫자가 들어가면 SyntaxError: Unexpected number in JSON at position 49 라는 오류가 발생 이유는 모름.
-            content,
+        const post = await Posts.findById(postId);
+        if (!post) {
+            return res.status(403).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        }
+        await Comments.create({
+            postId: postId,
+            userId: _id,
+            nickname: nickname,
+            content: content,
         });
-        res.status(200).send({ msg: '댓글을 생성하였습니다.' });
+        res.status(200).json({ message: '댓글이 성공적으로 작성되었습니다.' });
     } catch (error) {
-        if (!content) {
-            res.status(400).send({ msg: '댓글 내용을 입력해 주세요.' });
-        }
-        if (postId !== Posts.findOne({ postId })) {
-            res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
-        }
+        console.log(error.message);
+        res.status(400).json({ errorMessage: '댓글 작성에 실패하였습니다.' });
     }
 });
 
-// 댓글 목록 조회
-router.get('/comments/:postId', async (req, res) => {
+// 2. 댓글 목록 조회
+router.get('/:postId/comments', async (req, res) => {
     const { postId } = req.params;
-    console.log(postId);
     try {
-        const comment = await Comments.findOne({ postId }).sort({ createdAt: -1 });
-        console.log(comment);
-        res.json({ data: `${JSON.stringify(comment)}` });
+        const post = Posts.findOne({ _id: postId });
+        if (!post) {
+            return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        }
+        const comments = Comments.find({ postId: postId });
+        const data = {
+            comments: comments.map((a) => {
+                return {
+                    commentId: a._id,
+                    userId: a.userId,
+                    comment: a.comment,
+                    nickname: a.nickname,
+                    createdAt: a.createdAt,
+                    updatedAt: a.updatedAt,
+                };
+            }),
+        };
+        res.status(200).json(data);
     } catch (error) {
-        res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
+        console.log(error.message);
+        res.status(400).json({ errorMessage: '댓글 조회에 실패하였습니다.' });
     }
 });
 
 // 댓글 수정
-router.put('/comments/:_id', async (req, res) => {
+router.put('/:postId/comments/:commentId', async (req, res) => {
     const { _id } = req.params;
     console.log(_id);
     const { password, content } = req.body;
@@ -62,7 +86,7 @@ router.put('/comments/:_id', async (req, res) => {
 });
 
 // 댓글 삭제
-router.delete('/comments/:_id', async (req, res) => {
+router.delete('/:postId/comments', async (req, res) => {
     const { _id } = req.params;
     const { password } = req.body;
     const existComment = await Comments.findOne({ _id });
