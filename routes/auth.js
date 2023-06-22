8fbdf9f488b50b330202e8bf0160413fafd43c34
yooonzero <playlist808@kakao.com>
2023-06-22 17:15:14 +0900

    회원가입시, 닉네임 구성요소 확인기능 추가, 패스워드 길이제한 추가, 패스워드와 닉네임값이 동일한지 체크

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,21 @@ const jwt = require('jsonwebtoken');
 router.post('/auth', async (req, res) => {
     const { nickname, password, confirm } = req.body;
     try {
+        // 닉네임 구성요소 확인
+        const confirmedNickname = /^[a-zA-Z0-9]{3,}$/.test(nickname);
+        // ^: 문자열의 시작을 의미
+        // [a-zA-Z0-9]: 영어 대소문자와 숫자로 구성
+        // {3,}: 3글자 이상
+        // $: 문자열의 끝을 의미
+        // test() 메서드는 정규식과 nickname을 비교, 만족여부에 따라 true || false Boolean 값을 반환하고, 해당 값을 선언된 변수에 할당.
+        // confirmedNickname는 true 또는 false 값을 가지게 된다.
+        if (!confirmedNickname) {
+            res.status(412).json({
+                errorMessage: '닉네임의 형식이 올바르지 않습니다.',
+            });
+            return;
+        }
+
         // 닉네임 중복 확인
         const existNickname = await User.findOne({ nickname }); // User스키마를 통해서 DB에 이미 입력한 nickname 값이 존재하는지 확인하고,
         if (existNickname) {
@@ -15,6 +30,23 @@ router.post('/auth', async (req, res) => {
                 errorMessage: '이미 존재하는 닉네임 입니다.', // 에러메시지를 출력하고,
             });
             return; // 리턴시킨다.
+        }
+
+        //패스워드 길이 확인 : 4자이상
+        if (password.length < 4) {
+            res.status(412).json({
+                errorMessage: '패스워드의 형식이 올바르지 않습니다.',
+            });
+            return;
+        }
+
+        //패스워드의 값이 닉네임과 동일하지는 않는지 확인
+        // if (password.includes(...nickname)) // 한 글자라도 포함되어 있다면, 사용하지 못하게 하는 코드.
+        if (password.includes(nickname)) {
+            res.status(412).json({
+                errorMessage: '패스워드에 닉네임이 포함되어있습니다.',
+            });
+            return;
         }
 
         // 패스워드 확인
